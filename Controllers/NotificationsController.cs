d8f84f6584e72b85198755c7f685cb15228b7d73
Ricky0-0Tricky <yourmother_bel@hotmail.com>
2026-09-08 14:55:59 +0100

    initial creation of the notifications controller and respective views

---
+++ Controllers/NotificationsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Talk2Me.Data;
+using Talk2Me.Models;
+
+namespace Talk2Me.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public NotificationsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Notifications
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Notifications.Include(n => n.User);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Notifications/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notification = await _context.Notifications
+                .Include(n => n.User)
+                .FirstOrDefaultAsync(m => m.NotificationId == id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+
+            return View(notification);
+        }
+
+        // GET: Notifications/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "PasswordHash");
+            return View();
+        }
+
+        // POST: Notifications/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("NotificationId,Type,Content,IsRead,CreatedAt,UserId")] Notification notification)
+        {
+            if (ModelState.IsValid)
+            {
+                notification.NotificationId = Guid.NewGuid();
+                _context.Add(notification);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "PasswordHash", notification.UserId);
+            return View(notification);
+        }
+
+        // GET: Notifications/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notification = await _context.Notifications.FindAsync(id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "PasswordHash", notification.UserId);
+            return View(notification);
+        }
+
+        // POST: Notifications/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("NotificationId,Type,Content,IsRead,CreatedAt,UserId")] Notification notification)
+        {
+            if (id != notification.NotificationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(notification);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NotificationExists(notification.NotificationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "PasswordHash", notification.UserId);
+            return View(notification);
+        }
+
+        // GET: Notifications/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notification = await _context.Notifications
+                .Include(n => n.User)
+                .FirstOrDefaultAsync(m => m.NotificationId == id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+
+            return View(notification);
+        }
+
+        // POST: Notifications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var notification = await _context.Notifications.FindAsync(id);
+            if (notification != null)
+            {
+                _context.Notifications.Remove(notification);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NotificationExists(Guid id)
+        {
+            return _context.Notifications.Any(e => e.NotificationId == id);
+        }
+    }
+}
