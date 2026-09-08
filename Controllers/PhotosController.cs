9522829b04048b5c8d9d733283a6e16c6e4942b2
Ricky0-0Tricky <yourmother_bel@hotmail.com>
2026-09-08 15:00:59 +0100

    initial creation of the photos controller and respective views

---
+++ Controllers/PhotosController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Talk2Me.Data;
+using Talk2Me.Models;
+
+namespace Talk2Me.Controllers
+{
+    public class PhotosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PhotosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Photos
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Photos.Include(p => p.Comment);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Photos/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var photo = await _context.Photos
+                .Include(p => p.Comment)
+                .FirstOrDefaultAsync(m => m.PhotoId == id);
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            return View(photo);
+        }
+
+        // GET: Photos/Create
+        public IActionResult Create()
+        {
+            ViewData["CommentId"] = new SelectList(_context.Comments, "CommentId", "Content");
+            return View();
+        }
+
+        // POST: Photos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PhotoId,PhotoFile,CommentId")] Photo photo)
+        {
+            if (ModelState.IsValid)
+            {
+                photo.PhotoId = Guid.NewGuid();
+                _context.Add(photo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CommentId"] = new SelectList(_context.Comments, "CommentId", "Content", photo.CommentId);
+            return View(photo);
+        }
+
+        // GET: Photos/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var photo = await _context.Photos.FindAsync(id);
+            if (photo == null)
+            {
+                return NotFound();
+            }
+            ViewData["CommentId"] = new SelectList(_context.Comments, "CommentId", "Content", photo.CommentId);
+            return View(photo);
+        }
+
+        // POST: Photos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("PhotoId,PhotoFile,CommentId")] Photo photo)
+        {
+            if (id != photo.PhotoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(photo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PhotoExists(photo.PhotoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CommentId"] = new SelectList(_context.Comments, "CommentId", "Content", photo.CommentId);
+            return View(photo);
+        }
+
+        // GET: Photos/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var photo = await _context.Photos
+                .Include(p => p.Comment)
+                .FirstOrDefaultAsync(m => m.PhotoId == id);
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            return View(photo);
+        }
+
+        // POST: Photos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var photo = await _context.Photos.FindAsync(id);
+            if (photo != null)
+            {
+                _context.Photos.Remove(photo);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PhotoExists(Guid id)
+        {
+            return _context.Photos.Any(e => e.PhotoId == id);
+        }
+    }
+}
