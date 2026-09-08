06ec524d266fca4ab09699d53043bbfeb6e98d85
Ricky0-0Tricky <yourmother_bel@hotmail.com>
2026-09-08 09:09:34 +0100

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Talk2Me.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
