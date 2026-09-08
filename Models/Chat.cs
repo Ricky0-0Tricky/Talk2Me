99c2359f7e64145d836c03553a94f0d65090c8e5
Ricky0-0Tricky <yourmother_bel@hotmail.com>
2026-09-08 10:50:04 +0100

    added missing models

---
+++ Models/Chat.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Talk2Me.Models
+{
+    public class Chat
+    {
+        [Key]
+        [HiddenInput]
+        public Guid ChatId { get; set; }
+
+        [Required]
+        public string State { get; set; } = "Active";
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Talk2Me.Models
+{
+    public class Message
+    {
+        [Key]
+        [HiddenInput]
+        public Guid MessageId { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        public string Content { get; set; } = string.Empty;
+
+        [Display(Name = "Sent Date")]
+        public DateTimeOffset SendDate { get; set; } = DateTimeOffset.Now;
+
+        [Display(Name = "Read Date")]
+        public DateTimeOffset ReadDate { get; set; } 
+    }
+}
