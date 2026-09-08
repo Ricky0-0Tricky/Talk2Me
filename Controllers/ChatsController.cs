5519336267630cf6be4960c1be6826f4c055bf0d
Ricky0-0Tricky <yourmother_bel@hotmail.com>
2026-09-08 14:46:38 +0100

    initial creation of the chat controller and respective views

---
+++ Controllers/ChatsController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Talk2Me.Data;
+using Talk2Me.Models;
+
+namespace Talk2Me.Controllers
+{
+    public class ChatsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ChatsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Chats
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Chats.Include(c => c.UserA).Include(c => c.UserB);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Chats/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chat = await _context.Chats
+                .Include(c => c.UserA)
+                .Include(c => c.UserB)
+                .FirstOrDefaultAsync(m => m.ChatId == id);
+            if (chat == null)
+            {
+                return NotFound();
+            }
+
+            return View(chat);
+        }
+
+        // GET: Chats/Create
+        public IActionResult Create()
+        {
+            ViewData["UserAId"] = new SelectList(_context.Users, "UserId", "PasswordHash");
+            ViewData["UserBId"] = new SelectList(_context.Users, "UserId", "PasswordHash");
+            return View();
+        }
+
+        // POST: Chats/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ChatId,State,UserAId,UserBId")] Chat chat)
+        {
+            if (ModelState.IsValid)
+            {
+                chat.ChatId = Guid.NewGuid();
+                _context.Add(chat);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserAId"] = new SelectList(_context.Users, "UserId", "PasswordHash", chat.UserAId);
+            ViewData["UserBId"] = new SelectList(_context.Users, "UserId", "PasswordHash", chat.UserBId);
+            return View(chat);
+        }
+
+        // GET: Chats/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chat = await _context.Chats.FindAsync(id);
+            if (chat == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserAId"] = new SelectList(_context.Users, "UserId", "PasswordHash", chat.UserAId);
+            ViewData["UserBId"] = new SelectList(_context.Users, "UserId", "PasswordHash", chat.UserBId);
+            return View(chat);
+        }
+
+        // POST: Chats/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("ChatId,State,UserAId,UserBId")] Chat chat)
+        {
+            if (id != chat.ChatId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(chat);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ChatExists(chat.ChatId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserAId"] = new SelectList(_context.Users, "UserId", "PasswordHash", chat.UserAId);
+            ViewData["UserBId"] = new SelectList(_context.Users, "UserId", "PasswordHash", chat.UserBId);
+            return View(chat);
+        }
+
+        // GET: Chats/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chat = await _context.Chats
+                .Include(c => c.UserA)
+                .Include(c => c.UserB)
+                .FirstOrDefaultAsync(m => m.ChatId == id);
+            if (chat == null)
+            {
+                return NotFound();
+            }
+
+            return View(chat);
+        }
+
+        // POST: Chats/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var chat = await _context.Chats.FindAsync(id);
+            if (chat != null)
+            {
+                _context.Chats.Remove(chat);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ChatExists(Guid id)
+        {
+            return _context.Chats.Any(e => e.ChatId == id);
+        }
+    }
+}
